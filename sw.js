4bac8669bb3ec89440597b09d3407ea54b751595
Philipp Semsky <Captainpast@gmx.de>
2020-05-14 17:57:36 +0200

    register resoures

--- sw.js
+++ sw.js
@@ -1,1 +1,23 @@
 //JavaScript by Captinpast
+var cacheName = "v0.0.1"
+var resoures = [
+  "/",
+  "index.html",
+  "data/main.js",
+  "data/main.css",
+  "data/icons/cpa.png",
+]
+
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(resoures);
+    })
+  )
+})
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(caches.match(event.request).then(function(cachedResponse) {
+    return cachedResponse || fetch(event.request);
+  }))
+})
